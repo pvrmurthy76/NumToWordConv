95864c33c943e6f8ea9be1d0917b394d03b24eca
pvrmurthy76 <51827595+pvrmurthy76@users.noreply.github.com>
2019-07-14 11:09:28 +0530

    Update AppTest.java
    Formatted the code

--- numtoword/src/test/java/com/example/numtoword/AppTest.java
+++ numtoword/src/test/java/com/example/numtoword/AppTest.java
@@ -16,55 +16,56 @@ import com.example.numtoword.service.NumToWordConvertImpl;
 public class AppTest 
 {
 	NumToWordConvert util = new NumToWordConvertImpl();
-	
-    @Test
+
+	@Test
 	public void getWordFormatForCorrectNumber() {
 		int numVal = 1234;
 		assertEquals("One Thousand Two Hundred and Thirty Four ", util.convertToWord(numVal));
 	}
-    
-   @Test
-   public void validateInputLength() {
+
+	@Test
+	public void validateInputLength() {
 		int numVal = 1234567890;
 		String resp = util.convertToWord(numVal);
 		assertTrue(resp.contains("Invalid"));
 	}
-   
-    @Test
-    public void validateNegativeNumbers() {
+
+	@Test
+	public void validateNegativeNumbers() {
 		int numVal = -12345678;
 		String resp = util.convertToWord(numVal);
 		assertTrue(resp.contains("Invalid"));
 	}
-    
-    @Test
-    public void validateSingleDigits() {
-    	int numVal = 1;
-    	assertEquals("One ", util.convertToWord(numVal));
-    }
-    
-    @Test
-    public void validateForZero() {
-    	int numVal = 0000000;
-    	assertEquals("Zero ", util.convertToWord(numVal));
-    }
-    
-    @Test
-    public void validateForTeenDigits() {
-    	int numVal = 16;
-    	assertEquals("Sixteen ", util.convertToWord(numVal));
-    }
-    
-    @Test
-    public void validateForHundreds() {
-    	int numVal = 102;
-    	assertEquals("One Hundred and Two ", util.convertToWord(numVal));
-    }
-    
-    @Test
-    public void validateForMillions() {
-    	int numVal = 56945781;
-    	assertEquals("Fifty Six Million Nine Hundred and Forty Five Thousand Seven Hundred and Eighty One ", util.convertToWord(numVal));
-    }
-	
+
+	@Test
+	public void validateSingleDigits() {
+		int numVal = 1;
+		assertEquals("One ", util.convertToWord(numVal));
+	}
+
+	@Test
+	public void validateForZero() {
+		int numVal = 0000000;
+		assertEquals("Zero ", util.convertToWord(numVal));
+	}
+
+	@Test
+	public void validateForTeenDigits() {
+		int numVal = 16;
+		assertEquals("Sixteen ", util.convertToWord(numVal));
+	}
+
+	@Test
+	public void validateForHundreds() {
+		int numVal = 102;
+		assertEquals("One Hundred and Two ", util.convertToWord(numVal));
+	}
+
+	@Test
+	public void validateForMillions() {
+		int numVal = 56945781;
+		assertEquals("Fifty Six Million Nine Hundred and Forty Five Thousand Seven Hundred and Eighty One ",
+				util.convertToWord(numVal));
+	}
+
 }
